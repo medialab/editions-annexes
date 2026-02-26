727365690b9811c62f97e34f98d20743a4c698e2
TommasoPrinetti <Tommi.prinetti@gmail.com>
2026-02-26 17:13:04 +0100

    fixed og-image

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -21,11 +21,11 @@
 	<meta property="og:title" content={pageTitle} />
 	<meta property="og:description" content={pageDescription} />
 	<meta property="og:url" content={page.url.href} />
-	<meta property="og:image" content={socialImageUrl} />
 	<meta property="og:image:alt" content={pageTitle} />
 	<meta name="twitter:title" content={pageTitle} />
 	<meta name="twitter:description" content={pageDescription} />
-	<meta name="twitter:image" content={socialImageUrl} />
+	<meta property="og:image" content="og_image.png" />
+	<meta name="twitter:image" content="og_image.png" />
 </svelte:head>
 
 <main
